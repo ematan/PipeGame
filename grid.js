ed3a23285b439066ea6277030eb454e7b85c1e94
AnuVehkamaeki <anu.vehkis@gmail.com>
2017-04-21 13:17:03 +0300

    aloitus

---
+++ grid.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    
+    var drawGrid = function(w, h, id) {
+        var canvas = document.getElementById("game");
+        var ctx = canvas.getContext('2d');
+        ctx.canvas.width  = w;
+        ctx.canvas.height = h;
+
+
+        for (x=0;x<=w;x+=20) {
+            for (y=0;y<=h;y+=20) {
+                ctx.moveTo(x, 0);
+                ctx.lineTo(x, h);
+                ctx.stroke();
+                ctx.moveTo(0, y);
+                ctx.lineTo(w, y);
+                ctx.stroke();
+            }
+        }
+
+        };
+
+        drawGrid(800, 400, "grid");
+
+})
