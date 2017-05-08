2ad81c9df07c3d70fbcae240974b5597f282bede
AnuVehkamaeki <anu.vehkis@gmail.com>
2017-05-08 13:39:17 +0300

    huoh

--- startGame.js
+++ startGame.js
@@ -1,10 +1,11 @@
-var startGame = function(game){}
+var startGame = function(game){console.log("testi2")}
  
 startGame.prototype = {
   	create: function(){
-		var gameTitle = this.game.add.sprite(160,160,"Rakenna oma Balticconnector!");
+        console.log("testi")
+		var gameTitle = this.game.add.sprite(160,160,"straight1");
 		gameTitle.anchor.setTo(0.5,0.5);
-		var playButton = this.game.add.button(160,320,"pelaa",this.playTheGame,this);
+		var playButton = this.game.add.button(160,320,"straight1",this.playTheGame,this);
 		playButton.anchor.setTo(0.5,0.5);
 	},
 	playTheGame: function(){
