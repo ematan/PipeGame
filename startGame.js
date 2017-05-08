6d337df003896890fba9cd75df76774f77e082eb
AnuVehkamaeki <anu.vehkis@gmail.com>
2017-05-08 13:04:11 +0300

    lo

--- startGame.js
+++ startGame.js
@@ -1,18 +1,13 @@
-var startGame = {
-    
-    create: function(){
-        
-        var nameLabel = game.add.text(100,100, 'Rakenna oma Balticconnector!', {font: 'bold 30px Annie Use Your Telescope', fill: '#ff2626' });
-        
-        var startLabel = game.add.text(100, 200, 'paina w-nappulaa aloittaaksesi', {font: 'bold 30px Annie Use Your Telescope', fill: '#ff2626' });
-        
-        var wkey = game.input.keyboard.addkey(Phaser.Keyboard.W);
-        
-        wkey.onDown.addOnce(this.start, this);
-    }
-    
-    start: function(){
-        game.state.start('level1');
-    }
-    
+var startGame = function(game){}
+ 
+startGame.prototype = {
+  	create: function(){
+		var gameTitle = this.game.add.sprite(160,160,"Rakenna oma Balticconnector!");
+		gameTitle.anchor.setTo(0.5,0.5);
+		var playButton = this.game.add.button(160,320,"pelaa",this.playTheGame,this);
+		playButton.anchor.setTo(0.5,0.5);
+	},
+	playTheGame: function(){
+		this.game.state.start("level1");
+	}
 }
