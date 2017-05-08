be3032f3a157568dc4a074c4c3219cb43e14d964
ematan <emmuskah@gmail.com>
2017-05-08 13:08:58 +0300

    koklaus

---
+++ bootpreload.js
@@ -0,0 +1,40 @@
+var boot = function(game){
+	console.log("%cKäynnistä peli", "color:white; background:red");
+};
+
+boot.prototype = {
+	preload: function(){
+          //latauskuva?? this.game.load.image("loading","assets/loading.png");
+	},
+  	create: function(){
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+		//this.scale.setScreenSize();
+		this.game.state.start("Preload");
+	}
+}
+
+
+//preload alkaa tästä
+var preload = function(game){}
+
+preload.prototype = {
+	preload: function(){
+          //var loadingBar = this.add.sprite(160,240,"loading");
+          //loadingBar.anchor.setTo(0.5,0.5);
+          //this.load.setPreloadSprite(loadingBar);
+          this.game.stage.backgroundColor = '';
+          this.game.load.image('start', 'putket/start.png');
+          this.game.load.image('straight1', 'putket/straight1.png');
+          this.game.load.image('straight2', 'putket/straight2.png');
+          this.game.load.image('corner1', 'putket/corner1.png');
+          this.game.load.image('corner2', 'putket/corner2.png');
+          this.game.load.image('corner3', 'putket/corner3.png');
+          this.game.load.image('corner4', 'putket/corner4.png');
+          this.game.load.image('menu', 'valikko.png');
+          this.game.load.audio('mystery', 'mystery.mp3');
+	},
+  	create: function(){
+		this.game.state.start("startGame");
+	}
+}
---
+++ level1.js
@@ -0,0 +1,21 @@
+var level1 = function(game){}
+
+  level1.prototype = {
+    preload: function() {
+        this.game.stage.backgroundColor = '';
+    }
+    create: function() {
+          drawGrid(1.5*squareSize, 0, gridW, gridH);
+          drawGrid(0, 0, squareSize, gridH);
+          var pipesInGame = this.game.add.group();
+          var pipesWaiting = this.game.add.group();
+          startingPipe = this.game.add.sprite(140, 350, 'start');
+          endingPipe = this.game.add.sprite(840, 70, 'straight2');
+          createMega();
+          fillWaitingRoom();
+          drawWaitingRoom();
+          createTimer();
+          createPause();
+          createMusic();
+    }
+  }
