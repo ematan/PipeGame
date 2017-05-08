9477aa95399b7623be2f0a2212ae35eda8755474
AnuVehkamaeki <anu.vehkis@gmail.com>
2017-05-08 14:43:43 +0300

    level2

--- level2.js
+++ level2.js
@@ -1,1 +1,35 @@
 var level2 = function(game){}
+
+  level2.prototype = {
+    preload: function() {
+        this.game.stage.backgroundColor = '';
+        updateW = false;
+    },
+    create: function() {
+          drawGrid(1.5*squareSize, 0, gridW, gridH, this.game);
+          drawGrid(0, 0, squareSize, gridH, this.game);
+          var pipesInGame = this.game.add.group();
+          var pipesWaiting = this.game.add.group();
+          startingPipe = this.game.add.sprite(140, 350, 'start');
+          endingPipe = this.game.add.sprite(840, 70, 'straight2');
+          createMega();
+          fillWaitingRoom();
+          
+          drawWaitingRoom(this.game);
+          //createTimer();
+          //createPause(this.game);
+          //createMusic(this.game);
+    },
+    update: function(){
+        var a = this.game;
+        this.game.input.onDown.add(function () {updateY(a);}, this)
+        //fillWaitingRoom();
+        if(updateW){
+          drawWaitingRoom(this.game);
+          updateW =false
+        }
+    },
+    render: function(){
+        //game.debug.text('DEADLINE LÄHESTYY : ' + timer.duration.toFixed(0), 100, 525, { font: 'bold 25pt Annie Use Your Telescope' , fill: '#ff2626'});
+    }
+  };
