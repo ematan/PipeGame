11725fe8eb09a549c172d30c6fb89159b5089d09
AnuVehkamaeki <anu.vehkis@gmail.com>
2017-05-08 16:02:45 +0300

    nappulat

--- startGame.js
+++ startGame.js
@@ -2,9 +2,21 @@ var startGame = function(game){}
 
 startGame.prototype = {
   	create: function(){
-		var playButton = this.game.add.button(this.game.world.centerX,this.game.world.centerY,"aloitaPeli",this.playTheGame,this);
-        playButton.scale.setTo(0.7, 0.7);
-		playButton.anchor.setTo(0.5,0.5);
+		var play1 = this.game.add.button(this.game.world.centerX,this.game.world.centerY-200,"aloitaPeli",this.playTheGame,this);
+        play1.scale.setTo(0.6, 0.6);
+		play1.anchor.setTo(0.5,0.5);
+        
+        var play2 = this.game.add.button(this.game.world.centerX,this.game.world.centerY-100,"aloitaPeli",this.playTheGame,this);
+        play2.scale.setTo(0.6, 0.6);
+		play2.anchor.setTo(0.5,0.5);
+        
+        var play3 = this.game.add.button(this.game.world.centerX,this.game.world.centerY,"aloitaPeli",this.playTheGame,this);
+        play3.scale.setTo(0.6, 0.6);
+		play3.anchor.setTo(0.5,0.5);
+        
+        var äänet = this.game.add.button(this.game.world.centerX,this.game.world.centerY+100,"aloitaPeli",this.playTheGame,this);
+        äänet.scale.setTo(0.6, 0.6);
+		äänet.anchor.setTo(0.5,0.5);
 	},
 	playTheGame: function(){
 		this.game.state.start("level1");
