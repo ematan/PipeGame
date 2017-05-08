345ac4200af40363dcf7f90ccce5ae39591610d4
AnuVehkamaeki <anu.vehkis@gmail.com>
2017-05-08 16:08:00 +0300

    nappulatoiminnallisuuksia

--- startGame.js
+++ startGame.js
@@ -2,23 +2,32 @@ var startGame = function(game){}
 
 startGame.prototype = {
   	create: function(){
-		var play1 = this.game.add.button(this.game.world.centerX,this.game.world.centerY-200,"aloitaPeli",this.playTheGame,this);
+		var play1 = this.game.add.button(this.game.world.centerX,this.game.world.centerY-200,"aloitaPeli",this.playLevel1,this);
         play1.scale.setTo(0.6, 0.6);
 		play1.anchor.setTo(0.5,0.5);
         
-        var play2 = this.game.add.button(this.game.world.centerX,this.game.world.centerY-100,"aloitaPeli",this.playTheGame,this);
+        var play2 = this.game.add.button(this.game.world.centerX,this.game.world.centerY-100,"aloitaPeli",this.playLevel2,this);
         play2.scale.setTo(0.6, 0.6);
 		play2.anchor.setTo(0.5,0.5);
         
-        var play3 = this.game.add.button(this.game.world.centerX,this.game.world.centerY,"aloitaPeli",this.playTheGame,this);
+        var play3 = this.game.add.button(this.game.world.centerX,this.game.world.centerY,"aloitaPeli",this.playLevel3,this);
         play3.scale.setTo(0.6, 0.6);
 		play3.anchor.setTo(0.5,0.5);
         
-        var äänet = this.game.add.button(this.game.world.centerX,this.game.world.centerY+100,"aloitaPeli",this.playTheGame,this);
+        var äänet = this.game.add.button(this.game.world.centerX,this.game.world.centerY+100,"aloitaPeli",this.soundCheck,this);
         äänet.scale.setTo(0.6, 0.6);
 		äänet.anchor.setTo(0.5,0.5);
 	},
-	playTheGame: function(){
+	playLevel1: function(){
 		this.game.state.start("level1");
+	},
+    playLevel2: function(){
+		this.game.state.start("level2");
+	},
+    playLevel3: function(){
+		this.game.state.start("level3");
+	},
+    soundCheck: function(){
+		
 	}
 }
