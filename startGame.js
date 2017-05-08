e7332a181b3aba8cca8eb17e02e6eb37eb4fed25
ematan <emmuskah@gmail.com>
2017-05-08 14:41:33 +0300

    jh

--- startGame.js
+++ startGame.js
@@ -1,11 +1,10 @@
-var startGame = function(game){console.log("testi2")}
- 
+var startGame = function(game){}
+
 startGame.prototype = {
   	create: function(){
-        console.log("testi")
-		var gameTitle = this.game.add.sprite(160,160,"straight1");
+		var gameTitle = this.game.add.sprite(160,160,"Rakenna oma Balticconnector!");
 		gameTitle.anchor.setTo(0.5,0.5);
-		var playButton = this.game.add.button(160,320,"straight1",this.playTheGame,this);
+		var playButton = this.game.add.button(160,320,"pelaa",this.playTheGame,this);
 		playButton.anchor.setTo(0.5,0.5);
 	},
 	playTheGame: function(){
